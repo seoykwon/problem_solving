150c2039a330634be781f926f123db4f494e424e
seoykwon <kwonseoy@gmail.com>
2024-05-03 23:14:08 +0900

    code optimization (time complexity)

--- 1859.py
+++ 1859.py
@@ -3,32 +3,41 @@ sys.stdin = open("input.txt", "r")
 # sys.stdout = open("output.txt", "w")
 
 def solution():
+    # profit = 0
+    # start_index = 0
+    # max_index = 0
+    # max_ = 0
+    # now_buy_cost = 0
+    # count = 0
+    # while True:
+
+    #     for i in range(start_index, N):
+    #         if max_ < cost[i]:
+    #             max_ = cost[i]
+    #             max_index = i
+
+    #     for i in range(start_index, max_index):
+    #         now_buy_cost += cost[i]
+    #         count += 1
+
+    #     profit += (cost[max_index] * count) - now_buy_cost
+    #     count = 0
+    #     now_buy_cost = 0
+    #     max_ = 0
+    #     if max_index == len(cost) - 1:
+    #         break
+
+    #     start_index = max_index + 1
+
+    # return profit
     profit = 0
-    start_index = 0
-    max_index = 0
-    max_ = 0
-    now_buy_cost = 0
-    count = 0
-    while True:
 
-        for i in range(start_index, N):
-            if max_ < cost[i]:
-                max_ = cost[i]
-                max_index = i
-
-        for i in range(start_index, max_index):
-            now_buy_cost += cost[i]
-            count += 1
-
-        profit += (cost[max_index] * count) - now_buy_cost
-        count = 0
-        now_buy_cost = 0
-        max_ = 0
-        if max_index == len(cost) - 1:
-            break
-
-        start_index = max_index + 1
-
+    max_ = cost[-1]
+    for i in range(-2, -(1 + N), -1):
+        if max_ < cost[i]:
+            max_ = cost[i]
+        else:
+            profit += (max_ - cost[i])
     return profit
     
 
