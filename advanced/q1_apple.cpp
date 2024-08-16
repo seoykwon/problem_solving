e65d72a28efac4ac5b71a96e1b6d9da1b0fc1628
unknown <kwonseoy@gmail.com>
2024-08-16 17:32:56 +0900

    .

--- advanced/q1_apple.cpp
+++ advanced/q1_apple.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <queue>
+#include <climits>
 
 using namespace std;
 
@@ -8,11 +9,13 @@ int n;
 int field[10][10] = {
     0,
 };
+int maxApple;
 
 void input()
 {
     cin >> n;
 
+    maxApple = INT_MIN;
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
@@ -26,6 +29,11 @@ void input()
         for (int j = 0; j < n; j++)
         {
             cin >> field[i][j];
+
+            if (field[i][j] > maxApple)
+            {
+                maxApple = field[i][j];
+            }
         }
     }
 }
