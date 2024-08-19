a1a710869cb8c69295ab776ec08e08259043b39a
unknown <kwonseoy@gmail.com>
2024-08-19 17:53:26 +0900

    A q2

--- advanced/q2.cpp
+++ advanced/q2.cpp
@@ -1,89 +1,109 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-int t, n;
-int cnt;
-int board[51][51] = {0};
-int s = 0;
-
-struct po{
-    int y;
-    int x;
-    int num;
-};
-struct jjol{
-    int y;
-    int x;
-    int num;
+int T, N;
+int board[51][51];
+int cnt = 0;
+int po_x = 0;
+int po_y = 0;
+int visited[51][51] = {
+    0,
 };
 
-po p;
-vector <jjol> jj;
+void input()
+{
+    cin >> N;
 
-void input(){
-    cin >> n;
-
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < n; j++){
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
             board[i][j] = 0;
         }
     }
-
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < n; j++){
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
             cin >> board[i][j];
 
-            if (board[i][j] == 2){
-                p = {i, j, 2};
-            }
-            else if (board[i][j] == 1){
-                jj.push_back({i, j, 1});
+            if (board[i][j] == 2)
+            {
+                po_y = i;
+                po_x = j;
+                board[i][j] = 0;
             }
         }
     }
 }
 
-int dfs(int y, int x){
-    if (board[y][x] == 1){
-        board[y][x] = 0;
-        return 1;
+void dfs(int y, int x, int trial)
+{
+
+    if (trial == 4)
+    {
+        return;
     }
-    
-    int trial = 3;
+    if (board[y][x] == 1)
+    {
 
-    int ydir[4] = {1, -1, 0, 0};
-    int xdir[4] = {0, 0, -1, 1};
+        // cout << "eat jjol: " << y << " " << x << endl;
+        cnt++;
+        // cout << "cnt: " << cnt << endl;
+        board[y][x] = 3;
+        return;
+    }
+    int ydir[4] = {-1, 0, 1, 0};
+    int xdir[4] = {0, -1, 0, 1};
 
-    while(trial--){
-        for (int i = 0; i < 4; i++){
+    for (int i = 0; i < 4; i++)
+    {
+        bool can_j = false;
+        for (int j = 1; j < N; j++)
+        {
+            int ny = y + ydir[i] * j;
+            int nx = x + xdir[i] * j;
 
-            int ny = y + ydir[i];
-            int nx = x + xdir[i];
+            if (ny < 0 || nx < 0 || ny >= N || nx >= N)
+                break;
 
-            if (ny < 0 || nx < 0 || ny >= n || nx >= n) continue;
+            if ((board[ny][nx] == 1 || board[ny][nx] == 3) && !can_j)
+            {
+                // int nny = y + ydir[i] * (j + 1);
+                // int nnx = x + xdir[i] * (j + 1);
 
-            if(board[ny][nx] == 1){
-                
+                // if (nny < 0 || nnx < 0 || nny >= N || nnx >= N)
+                //     break;
+
+                // if (board[nny][nnx] == 0)
+                // {
+                //     can_j = true;
+                // }
+                can_j = true;
+                continue;
             }
-         
+
+            if (can_j)
+            {
+                cout << ny << " " << nx << " trial: " << trial << endl;
+                dfs(ny, nx, trial + 1);
+            }
         }
     }
 }
 
-int main(){
-    cin >> t;
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    cin >> T;
 
-    for (int i = 1; i <= t; i++){
-        cnt = 0;
+    for (int t = 1; t <= T; t++)
+    {
         input();
 
-        s = jj.size();
-
-        for (int j = 0; j < s; j++){
-            cnt += dfs(p.y, p.x);
-        }
+        dfs(po_y, po_x, 0);
 
         cout << "#" << t << " " << cnt << endl;
     }
