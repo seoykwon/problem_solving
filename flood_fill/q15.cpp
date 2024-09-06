f8daf3dcffa840e4bbb155c8079bb096a5d80544
권서영 <kwonseoy@gmail.com>
2024-09-06 11:03:42 +0900

    q15 solved

--- flood_fill/q15.cpp
+++ flood_fill/q15.cpp
@@ -66,6 +66,7 @@ void bfs(int y, int x){
         }
         else {
             // cout << bomb << endl;
+            visited[now.y][now.x] = true;
             res[now.y][now.x] = '0' + bomb;
         }
         
@@ -73,7 +74,7 @@ void bfs(int y, int x){
 }
 
 int main(){
-    freopen("input.txt", "r", stdin);
+    // freopen("input.txt", "r", stdin);
     cin >> N >> M;
     for (int i = 0; i < N; i++){
         for (int j = 0; j < M; j++){
@@ -93,4 +94,85 @@ int main(){
         }
         cout << endl;
     }
+}
+
+// ########## 강사님 코드 ###############
+#include <iostream>
+#include <queue>
+using namespace std;
+
+struct Node {
+    int r, c;
+    char type;
+};
+
+int H, W;
+char MAP[50][50];
+char result[50][50];
+int X, Y;
+void input() {
+    cin >> H >> W;
+    for (int r = 0; r < H; r++){
+        cin >> MAP[r];
+    }
+    cin >> X >> Y;
+}
+
+int dr[] = { -1,-1,-1,0,0,1,1,1 };
+int dc[] = { -1,0,1,-1,1,-1,0,1 };
+
+void change(Node now) {
+    if (MAP[now.r][now.c] == 'M'){
+        MAP[now.r][now.c] = 'X';
+        return;
+    }
+
+    int cnt = 0;
+    for (int t = 0; t < 8; t++){
+        int nr = now.r + dr[t];
+        int nc = now.c + dc[t];
+        if (nr < 0 || nc < 0 || nr >= H || nc >= W) continue;
+        if (MAP[nr][nc] == 'M'){
+            cnt++;
+        }
+    }
+    MAP[now.r][now.c] = cnt + '0';
+    if (MAP[now.r][now.c] == '0') MAP[now.r][now.c] = 'B';
+}
+
+void bfs(int r, int c){
+    queue<Node> qu;
+    int visited[50][50] = {0};
+    qu.push({r, c, MAP[r][c]});
+    visited[r][c] = 1;
+
+    while (!qu.empty()) {
+        Node now = qu.front(); qu.pop();
+        change(now);
+        if (MAP[now.r][now.c] == 'X') break;
+        if (MAP[now.r][now.c] != 'B') continue;
+
+        for (int t = 0; t < 8; t++) {
+            int nr = now.r + dr[t];
+            int nc = now.c + dc[t];
+
+            if (nr < 0 || nc < 0 || nr >= H || nc >= W) continue;
+            if (visited[nr][nc] == 1) continue;
+
+            visited[nr][nc] = 1;
+            qu.push({nr, nc});
+        }
+    }
+}
+
+int main(){
+    input();
+    bfs(X, Y);
+    for (int r = 0; r < H; r++){
+        for (int c = 0; c < W; c++){
+            cout << MAP[r][c];
+        }
+        cout << endl;
+    }
+    return 0;
 }
