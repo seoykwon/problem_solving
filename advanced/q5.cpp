b649bb346757452f9673b15a3801cbcce61faa40
권서영 <kwonseoy@gmail.com>
2024-10-16 17:47:14 +0900

    need to debug q5

--- advanced/q5.cpp
+++ advanced/q5.cpp
@@ -1,88 +1,75 @@
 #include <iostream>
-#include <stdlib.h>
-#include <queue>
-
+#include <cstring>
 using namespace std;
 
 int T;
 int N;
-struct tree
+int list[101];
+int diff[101];
+int target = 0;
+int sum = 0;
+
+int water()
 {
-    int height;
-    int day;
-};
-int ans = 0;
-int goal = 0;
-
-tree trees[101];
-
-void cnt()
-{
-    int today = 0;
-
     for (int i = 0; i < N; i++)
     {
-        if (trees[i].height == goal)
+        if (target == list[i])
             continue;
 
-        tree t = trees[i];
+        diff[i] = target - list[i];
 
-        while (1)
-        {
-            today++;
-
-            int diff = goal - t.height;
-
-            if (today % 2 != 0)
-            {
-                if (diff != 2)
-                    t.height += 1;
-                else
-                {
-                    trees[i + 1].height += 1;
-                }
-            }
-            else
-            {
-                if (diff != 1)
-                    t.height += 2;
-                else
-                {
-                    trees[i + 1].height += 2;
-                }
-            }
-
-            if (t.height == goal)
-            {
-                ans = max(ans, today);
-                break;
-            }
-        }
+        sum += diff[i];
     }
+
+    if (sum == 0)
+    {
+        return 0;
+    }
+
+    int res = sum / 2;
+    int rem = sum % 2;
+
+    int two, one;
+    two = res;
+    one = rem;
+
+    while (1)
+    {
+        if (abs(two - one) == 1)
+        {
+            break;
+        }
+        two--;
+        one += 2;
+    }
+
+    return (one + two + 1);
 }
 
 int main()
 {
     freopen("input.txt", "r", stdin);
+
     cin >> T;
 
     for (int tc = 1; tc <= T; tc++)
     {
+        memset(list, 0, sizeof(list));
+        memset(diff, 0, sizeof(diff));
         cin >> N;
-        ans = 0;
-        goal = 0;
+        target = 0;
+        sum = 0;
 
         for (int i = 0; i < N; i++)
         {
-            int tmp;
-            cin >> tmp;
-            trees[i] = {tmp, 0};
+            cin >> list[i];
 
-            goal = max(goal, tmp);
+            target = max(target, list[i]);
         }
 
-        cnt();
+        int ans = water();
 
         cout << "#" << tc << " " << ans << endl;
+        // cout << "# target" << " " << target << endl;
     }
 }
