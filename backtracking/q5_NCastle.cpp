9b3150d4bff31b5476e035a276ff7567e040f42b
seoykwon <kwonseoy@gmail.com>
2024-11-21 17:15:26 +0900

    N Castle

---
+++ backtracking/q5_NCastle.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int board[11][11];
+int n;
+int cnt = 0;
+int row[11];
+
+int dir[4][2] = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+
+void search(int num)
+{
+    if (num == n)
+    {
+        cnt++;
+        return;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        if (row[i])
+            continue;
+        row[i] = 1;
+        search(num + 1);
+        row[i] = 0;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    cin >> n;
+
+    search(0);
+
+    cout << cnt << '\n';
+    return 0;
+}
