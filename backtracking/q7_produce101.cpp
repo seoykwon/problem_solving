7a00cb7ad9b0d360b277937fe206e3baf670e10e
seoykwon <kwonseoy@gmail.com>
2024-11-23 20:19:54 +0900

    bt q7 debugging. use vector for char bt

--- backtracking/q7_produce101.cpp
+++ backtracking/q7_produce101.cpp
@@ -22,20 +22,25 @@ void go(int idx)
         return;
     }
 
+    int tmp = result;
+
     // multiplication
     result *= nums[idx];
+    operation[idx - 1] = '*';
     go(idx + 1);
-    result /= nums[idx];
+    result = tmp;
 
     // addition
     result += nums[idx];
+    operation[idx - 1] = '+';
     go(idx + 1);
-    result -= nums[idx];
+    result = tmp;
 
     // subtraction
     result -= nums[idx];
+    operation[idx - 1] = '-';
     go(idx + 1);
-    result += nums[idx];
+    result = tmp;
 }
 
 int main()
@@ -50,5 +55,72 @@ int main()
 
     go(1);
 
+    return 0;
+}
+
+// 첫 번째 코드의 문제는 전역 변수와 배열 초기화의 부정확함, 출력 로직의 복잡성 때문입니다.
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n;
+int nums[11];
+vector<char> path;
+long long result;
+
+void go(int idx)
+{
+    if (idx == n)
+    {
+        if (result % 101 == 0 && result != 0)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                cout << nums[i];
+                if (i < path.size())
+                    cout << path[i];
+            }
+            cout << "\n";
+        }
+        return;
+    }
+
+    long long tmp = result;
+
+    // 곱하기
+    result *= nums[idx];
+    path.push_back('*');
+    go(idx + 1);
+    result = tmp;
+    path.pop_back();
+
+    // 더하기
+    result += nums[idx];
+    path.push_back('+');
+    go(idx + 1);
+    result = tmp;
+    path.pop_back();
+
+    // 빼기
+    result -= nums[idx];
+    path.push_back('-');
+    go(idx + 1);
+    result = tmp;
+    path.pop_back();
+}
+
+int main()
+{
+    freopen("input.txt", "r", stdin);
+
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> nums[i];
+    }
+    result = nums[0];
+    go(1);
     return 0;
 }
