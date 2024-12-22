4d4efea56cd7782023c02d913cdb524007dcb7b5
seoykwon <74230007+seoykwon@users.noreply.github.com>
2024-12-22 12:17:52 +0900

    [Silver III] Title: 피보나치 함수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
@@ -4,30 +4,30 @@ using namespace std;
 
 int main()
 {
+    // freopen("input.txt", "r", stdin);
     int t;
     cin >> t;
 
     while (t--)
     {
-        int num;
-        cin >> num;
+        int n;
+        cin >> n;
 
-        int dp[num + 1][2] = {0};
-        // dp[i][0]: f(0) 호출 횟수, dp[i][1]: f(1) 호출 횟수
+        int dp[41][2] = {
+            0,
+        };
+        dp[0][0] = 1;
+        dp[0][1] = 0;
+        dp[1][0] = 0;
+        dp[1][1] = 1;
 
-        // 초기 조건 설정
-        dp[0][0] = 1; // f(0) 호출 시 f(0) = 1번 호출
-        dp[0][1] = 0; // f(0) 호출 시 f(1) = 0번 호출
-        dp[1][0] = 0; // f(1) 호출 시 f(0) = 0번 호출
-        dp[1][1] = 1; // f(1) 호출 시 f(1) = 1번 호출
-
-        for (int i = 2; i <= num; i++)
+        for (int i = 2; i <= n; i++)
         {
             dp[i][0] = dp[i - 1][0] + dp[i - 2][0];
             dp[i][1] = dp[i - 1][1] + dp[i - 2][1];
         }
 
-        cout << dp[num][0] << " " << dp[num][1] << '\n';
+        cout << dp[n][0] << " " << dp[n][1] << '\n';
     }
     return 0;
 }
