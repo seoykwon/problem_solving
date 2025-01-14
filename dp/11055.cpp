1b1b4f7910d29ece44be290f90a0d212d876679a
권서영 <kwonseoy@gmail.com>
2025-01-14 14:42:47 +0900

    .

--- dp/11055.cpp
+++ dp/11055.cpp
@@ -1,47 +1,35 @@
 #include <iostream>
-#include <algorithm>
-
 using namespace std;
+#define max(a, b) (a > b) ? a : b
 
-int n;
+int N;
 int arr[1001];
 int dp[1001];
 
-int main()
+int main(void)
 {
-    // freopen("input.txt", "r", stdin);
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+    cin >> N;
 
-    cin >> n;
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < N; i++)
         cin >> arr[i];
-    }
 
-    // DP 배열 초기화
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < N; i++)
     {
         dp[i] = arr[i];
-    }
-
-    int ans = 0;
-
-    // LIS 계산
-    for (int i = 1; i < n; i++)
-    {
         for (int j = 0; j < i; j++)
         {
-            if (arr[i] > arr[j])
-            {
+            // 증가하는 수열일 때, 합을 갱신한다.
+            if (arr[j] < arr[i])
                 dp[i] = max(dp[i], dp[j] + arr[i]);
-                // dp[i] = max(dp[i], dp[j] + dp[i]); 는 틀림
-            }
         }
-        ans = max(ans, dp[i]);
     }
 
-    cout << ans << '\n';
+    int ans = dp[0];
+    for (int i = 1; i < N; i++)
+        if (ans < dp[i])
+            ans = dp[i];
+
+    cout << ans;
 
     return 0;
 }
