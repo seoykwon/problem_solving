04bd025a6725559d6f506f935c8e5a62bddea8a7
권서영 <kwonseoy@gmail.com>
2025-01-20 18:00:19 +0900

    continue from here

---
+++ baekjoon/1030.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    freopen("input.txt", "r", stdin);
+
+    int s, N, K, R1, R2, C1, C2;
+    cin >> s >> N >> K >> R1 >> R2 >> C1 >> C2;
+}
