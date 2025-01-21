605a8caaa660e1a6461793f2ba921072c150acff
권서영 <kwonseoy@gmail.com>
2025-01-21 09:50:30 +0900

    resolve

--- baekjoon/1030.cpp
+++ baekjoon/1030.cpp
@@ -1,15 +1,49 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
+int s, N, K, R1, R2, C1, C2;
+
+void input()
+{
+    cin >> s >> N >> K >> R1 >> R2 >> C1 >> C2;
+}
+
+int solution(int len, int x, int y)
+{
+    int border;
+
+    if (len == 1)
+        return 0;
+    border = len / N;
+    if (x >= border * (N - K) / 2 && x < border * (N + K) / 2 && y >= border * (N - K) / 2 && y < border * (N + K) / 2)
+    {
+        return 1;
+    }
+    return solution(border, x % border, y % border);
+}
+
+void solve()
+{
+    int len = 1;
+
+    input();
+    while (s--)
+    {
+        len *= N;
+    }
+
+    for (int i = R1; i <= R2; ++i)
+    {
+        for (int j = C1; j <= C2; ++j)
+        {
+            cout << solution(len, i, j);
+        }
+        cout << '\n';
+    }
+}
+
 int main()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    freopen("input.txt", "r", stdin);
-
-    int s, N, K, R1, R2, C1, C2;
-    cin >> s >> N >> K >> R1 >> R2 >> C1 >> C2;
+    solve();
 }
