760e424a284bfdf61dd797ce4c3b5aa0e928297a
seoykwon <kwonseoy@gmail.com>
2025-03-12 16:54:16 +0900

    usage of stringstream

--- swea/힙+package.cpp
+++ swea/힙+package.cpp
@@ -6,7 +6,14 @@
 
 using namespace std;
 
-int main() {
+/**
+ * C++에서 cout을 사용하여 반복적으로 출력하는 경우, 각 출력마다 콘솔에 데이터를 바로 출력하게 됩니다.
+ * 이로 인해 입출력 속도가 상대적으로 느려질 수 있습니다.
+ * stringstream을 사용하면 중간 결과를 메모리 내의 문자열에 저장해두고, 마지막에 한 번에 출력할 수 있어 효율적입니다.
+ */
+
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
@@ -15,25 +22,31 @@ int main() {
 
     stringstream sb;
 
-    for (int tc = 1; tc <= TC; tc++) {
+    for (int tc = 1; tc <= TC; tc++)
+    {
         int N;
         cin >> N;
 
         sb << "#" << tc << " ";
-        
+
         priority_queue<int> pq;
 
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < N; i++)
+        {
             int op;
             cin >> op;
 
-            if (op == 1) {
+            if (op == 1)
+            {
                 int input;
                 cin >> input;
                 pq.push(input);
-            } else {
+            }
+            else
+            {
                 int output = -1;
-                if (!pq.empty()) {
+                if (!pq.empty())
+                {
                     output = pq.top();
                     pq.pop();
                 }
