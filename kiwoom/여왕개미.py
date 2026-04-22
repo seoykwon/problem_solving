2de1a98678657d8ebe9de61ebea52e2212f70693
seoykwon <kwonseoy@gmail.com>
2026-04-22 17:02:36 +0900

    greedy + circular linked list + binary search

--- kiwoom/여왕개미.py
+++ kiwoom/여왕개미.py
@@ -1,142 +1,86 @@
-import sys
-sys.stdin = open('input.txt', 'r')
-input = sys.stdin.readline
+# dic[번호] = [좌표, prev, next]
+# dic[0] 은 원형 연결 리스트의 더미 노드
+# dic[0] = [0(여왕 좌표=0), 마지막 집 번호, 첫 집 번호]
 
-import heapq
+def check(t, r):
+    left = t
+    ori_r = r
+    cur_num = dic[0][2]
+    
+    while r > 0:
+        # 개미 한마리 배치
+        r -= 1
 
-Q = int(input())
-N = 0
-cntNode = 0
+        left = t
 
-pos = {}
-rev_pos = {}
-valid = {}
-valid[0] = True # 여왕 개미
-prev = {}
-next = {}
-distance = [] # heap
+        # 다음 위치까지의 거리
+        while True:
+            next_num = dic[cur_num][2]
 
-def reset_visited(dispatch):
-    global visited
+            if next_num == 0 and left >= 0:
+                return True
+            length = dic[next_num][0] - dic[cur_num][0]
+            left -= length
+            if left >= 0:
+                cur_num = next_num
+            else:
+                cur_num = next_num
+                break
 
-    visited = [False] * (N)
+    return False
 
-    for d, a in dispatch:
-        visited[a] = True
-
-for _ in range(Q):
+q = int(input())
+dic = dict()
+for _ in range(q):
     query = list(map(int, input().split()))
     op = query[0]
 
     if op == 100:
-        # 마을 건설
-        N = query[1]
-        cntNode = N
-        lst = query[2:]
-        for i in range(N):
-            x = lst[i]
-            pos[i + 1] = x
-            rev_pos[x] = i + 1
-            valid[i + 1] = True
+        dic[0] = [0] * 3
+        dic[0][2] = 1
 
-            if i > 0:
-                prev[i + 1] = i
-                dist = pos[i + 1] - pos[i]
-                heapq.heappush(distance, (-(dist), i, i + 1))
-            else:
-                # i == 0, the very beginning
-                prev[i + 1] = -1
-                dist = pos[1]
-                heapq.heappush(distance, (-(dist), 0, i+1))
+        n = query[1]
+        dic[0][1] = n
+        for i, x in enumerate(query[2:]):
+            dic[i+1] = [0]*3
+            dic[i+1][0] = x
+            dic[i+1][1] = i
+            dic[i+1][2] = (i+2)%(n+1) # next (n번의 next는 0 = 여왕으로 돌아감)
 
-        # next도 연결해주기
-        for i in range(N, 1, -1):
-            next[i - 1] = i
-
-    if op == 200:
-        # 개미집 건설
+    elif op == 200:
+        n += 1
         p = query[1]
-        cntNode += 1
+        dic[n] = [0]*3
+        dic[n][0] = p
 
-        pos[N + 1] = p
-        rev_pos[p] = N + 1
-        valid[N + 1] = True
-        next[N + 1] = -1
-        idx = N
-        while True:
-            if valid[idx]:
-                prev[N + 1] = idx
-                next[idx] = N + 1 # next도 연결해주기
-                dist = pos[N + 1] - pos[idx]
-                heapq.heappush(distance, (-(dist), idx, N + 1))
-                break
-            idx -= 1
-        N += 1
+        b = dic[0][1]   # 현재 '마지막 집'의 번호 (여왕의 prev)
+        dic[b][2] = n   # 기존 마지막 집의 next = n
+        dic[n][1] = b   # n의 prev = 기존 마지막 집
+        dic[0][1] = n   # 여왕의 prev = n (n이 새 '마지막'이 됨)
+        dic[n][2] = 0   # n의 next = 여왕 (wrap)
 
-    if op == 300:
-        # 개미집 철거
+    elif op == 300:
         q = query[1]
-        cntNode -= 1
-
-        valid[q] = False
-        # 철거된 개미집이 맨 앞 (맨 왼쪽)
-        if prev[q] == -1:
-            nxt = next[q]
-            prev[nxt] = -1
-
-        # 철거된 개미집이 맨 뒤 (맨 오른쪽)
-        elif next[q] == -1:
-            pv = prev[q]
-            next[pv] = -1
-
-        # 철거된 개미집이 중간 위치
-        else:
-            a = prev[q]
-            b = next[q]
-
-            prev[b] = a
-            next[a] = b
-
-    if op == 400:
-        # 개미집 정찰
+        b = dic[q][1]
+        a = dic[q][2]
+        dic[b][2] = dic[q][2]
+        dic[a][1] = dic[q][1]
+        del dic[q]
+    
+    elif op == 400:
         r = query[1]
-        # 여왕 개미의 위치 = 0 도 선택할 수 있음
-        visited = [False] * N
-        # r 개밀들이 안전한 집으로 탐색한 집들 개수
-        cnt = 0
-        # heap에서 next 값과 valid 값을 체크하면 유효한 거리인지 알 수 있음
-        dispatch = []
-        tracker = 0
-        while distance:
-            antd, a, b = heapq.heappop(distance)
-            if valid[a] == False:
-                continue
-            if next[a] != b:
-                continue
-            dispatch.append([antd, a])
-            # visited[a] = True
+        left = 0
+        right = int(1e9)
+        answer = 0
 
-            if tracker == r:
-                break
-        cnt += len(dispatch)
+        while left <= right:
+            mid = (left + right) // 2
+            test = check(mid, r)
 
-        lo = 0
-        hi = pos[N]
-
-        while lo < hi:
-            reset_visited(dispatch)
-
-            mid = (lo + hi) // 2 # 소요 시간
-            # 어떻게 r 개미들을 적절한 위치에 배치해서 갯수를 세지? -> dispatch 사용
-            for d, a in dispatch:
-                for walk in range(1, mid):
-                    if rev_pos[a + walk] and valid[rev_pos[a + walk]] and visited[rev_pos[a + walk]] == False:
-                        visited[rev_pos[a + walk]] = True
-                        cnt += 1
-
-            if cnt < N:
-                lo = mid + 1
-            if cnt >= N:
-                hi = mid
-
-        print(hi)
+            if test:
+                answer = mid
+                right = mid - 1
+            else:
+                left = mid + 1
+        
+        print(answer)
