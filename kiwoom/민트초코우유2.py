f47460c16acda0a73ec17ded23526c392d1ff606
seoykwon <kwonseoy@gmail.com>
2026-05-28 19:23:16 +0900

    draft

--- kiwoom/민트초코우유2.py
+++ kiwoom/민트초코우유2.py
@@ -5,7 +5,7 @@ input = sys.stdin.readline
 from collections import deque
 import heapq
 
-f_order = [7, 6, 5, 3, 1, 2, 4]
+# f_order = [7, 6, 5, 3, 1, 2, 4]
 
 def morning(N, faith):
     for i in range(N):
@@ -13,7 +13,7 @@ def morning(N, faith):
             faith[i][j] += 1
 
 # bfs(N, food, faith, visited, represent, group, i, j, g_id)
-def bfs(N, food, faith, visited, represent, group, i, j, g_id):
+def bfs(N, food, faith, visited, represent, group, i, j, g_id, rset):
     q = deque()
     visited[i][j] = True
     q.append((i, j))
@@ -42,6 +42,7 @@ def bfs(N, food, faith, visited, represent, group, i, j, g_id):
     _, hr, hc = heapq.heappop(candidate)
     sz = len(candidate)
     represent[g_id] = [hr, hc]
+    rset.add((hr, hc))
 
     faith[hr][hc] += sz
 
@@ -52,16 +53,94 @@ def bfs(N, food, faith, visited, represent, group, i, j, g_id):
     return represent
 
 # noon(N, food, faith, represent, group)
-def noon(N, food, faith, represent, group):
+def noon(N, food, faith, represent, group, rset):
     g_id = 1
     visited = [[False]*N for _ in range(N)]
     for i in range(N):
         for j in range(N):
             if not visited[i][j]:
-                bfs(N, food, faith, visited, represent, group, i, j, g_id)
+                bfs(N, food, faith, visited, represent, group, i, j, g_id, rset)
                 g_id += 1
 
     return represent
+
+def spread(r, c, faith, food, N, rset):
+    sf = faith[r][c]
+    eagerness = sf - 1
+    d = sf % 4
+    defense = set()
+
+    DIR = [[-1,0],[1,0],[0,-1],[0,1]]
+    nr, nc = r, c
+
+    while True:
+        nr, nc = nr + DIR[d][0], nc + DIR[d][1]
+
+        if nr < 0 or nc < 0 or nr >= N or nc >= N:
+            break
+        if food[nr][nc] == food[r][c]:
+            continue
+
+        # 강한 전파 x > y
+        if sf > faith[nr][nc]:
+            food[nr][nc] = food[r][c]
+            eagerness = max(0, eagerness - (faith[nr][nc] + 1))
+            faith[nr][nc] += 1
+        # 약한 전파
+        elif sf <= faith[nr][nc]:
+            food[nr][nc] |= food[r][c]
+            eagerness = 0
+            faith[nr][nc] += sf
+        # 방어상태
+        if (nr, nc) in rset:
+            defense.add((nr, nc))
+
+    return defense
+
+
+def night(N, food, faith, represent, group, rset):
+    single = []
+    double = []
+    triple = []
+
+    for k, (rr, rc) in represent.items():
+        if food[rr][rc] in (4, 2, 1):
+            heapq.heappush(single, (-faith[rr][rc], rr, rc))
+        elif food[rr][rc] in (6, 5, 3):
+            heapq.heappush(double, (-faith[rr][rc], rr, rc))
+        elif food[rr][rc] == 7:
+            heapq.heappush(triple, (-faith[rr][rc], rr, rc))
+
+    df = set()
+
+    while single:
+        _, cr, cc = heapq.heappop(single)
+        if (cr, cc) in df:
+            continue
+        df = spread(cr, cc, faith, food, N, rset)
+
+    while double:
+        _, cr, cc = heapq.heappop(double)
+        if (cr, cc) in df:
+            continue
+        df = spread(cr, cc, faith, food, N, rset)
+
+    while triple:
+        _, cr, cc = heapq.heappop(triple)
+        if (cr, cc) in df:
+            continue
+        df = spread(cr, cc, faith, food, N, rset)
+
+def print_result(N, food, faith):
+    f_order = {7:0, 6:1, 5:2, 3:3, 1:4, 2:5, 4:6}
+    ans = [0]*7
+
+    for i in range(N):
+        for j in range(N):
+            idx = f_order[food[i][j]]
+            ans[idx] += faith[i][j]
+
+    print(*ans)
 
 def solve():
     N, T = map(int, input().split())
@@ -86,15 +165,23 @@ def solve():
 
     # print(*board)
     # print(*faith)
-    # 1. 아침시간
-    morning(N, faith)
+    while T:
+        # 1. 아침시간
+        morning(N, faith)
 
-    # 2. 점심시간
-    represent = {}
-    group = [[0]*N for _ in range(N)]
-    represent = noon(N, food, faith, represent, group)
+        # 2. 점심시간
+        represent = {}
+        rset = set()
+        group = [[0]*N for _ in range(N)]
+        represent = noon(N, food, faith, represent, group, rset)
 
-    print(*represent)
+
+        night(N, food, faith, represent, group, rset)
+
+        # print(*represent)
+        print_result(N, food, faith)
+
+        T -= 1
 
 solve()
 
